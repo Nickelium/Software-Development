3807fe8fc6baef855f697dd8ea11114a6c681369
Tom Houben <tomhouben@telenet.be>
2016-02-20 14:59:31 +0100

    BugReportId added

---
+++ src/Model/BugReportID.java
@@ -0,0 +1,25 @@
+package Model;
+
+import java.util.UUID;
+
+/**
+ * Created by Tom on 20/02/16.
+ */
+public class BugReportID {
+
+    private UUID Id;
+
+    public BugReportID(){
+        this.Id = UUID.randomUUID();
+    }
+
+    @Override
+    public String toString() {
+        return this.Id.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return this.Id.equals(obj);
+    }
+}
