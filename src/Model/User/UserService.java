cea563e5192ccf66e47b0bdaa92b063d43c20e15
Laurens Cleemput <laurenscleemput@hotmail.com>
2016-02-24 15:43:55 +0100

    Added more commentary in UserService class.

--- src/Model/User/UserService.java
+++ src/Model/User/UserService.java
@@ -55,7 +55,16 @@ public class UserService {
     }
 
 
-    // is de user name uniek?
+    /**
+     * Adds a new user to the user repository if there doesn't exist a user object with
+     * the same username in the User Repository yet.
+     *
+     * @throws IllegalArgumentException
+     * Throws an IllegalArgumentException in case there already exists a User object
+     * in the User Repository with the same username.
+     *
+     * @param user The user object that needs to be added to the user repository.
+     */
     public void addUser(User user){
         String userName =  user.getUserName();
         if (!repository.getAllMatching((s)->s.getUserName() == userName).isEmpty()){
@@ -65,14 +74,32 @@ public class UserService {
         }
     }
 
+    /**
+     * Updates a user object with given username with the instance of the user object
+     * in the user repository by calling an update statement of the repository.
+     *
+     * @param user the user object that needs to be updated in the user repository.
+     */
     public void updateUser(User user){
         repository.update(((s)->s.getUserName() == user.getUserName()), user);
     }
 
+    /**
+     * Deletes a given user object from the user repository by calling a delete
+     * statement of the repository.
+     *
+     * @param user the user object that needs to be deleted from the user repository.
+     */
     public void deleteUser(User user){
         repository.delete(user);
     }
 
+    /**
+     * Returns a user object from the user repository defined by the unique user name.
+     *
+     * @param userName the unique username of which we want to retrieve the user object from the user repository
+     * @return the user object from the user repository with given username.
+     */
     public User getUser(String userName){
         return repository.getOne((s)->s.getUserName()==userName);
     }
