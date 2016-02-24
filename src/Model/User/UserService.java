61decc72db8194a06d2dfe78e7c38ab43aefe75a
Karina Karapetyan <karina.st.vith@gmail.com>
2016-02-24 15:19:07 +0100

    UserService functions added

--- src/Model/User/UserService.java
+++ src/Model/User/UserService.java
@@ -3,8 +3,6 @@ import java.util.Collections;
 import java.util.List;
 import DAL.IRepository;
 
-import java.util.Collection;
-
 /**
  * Created by Karina on 19.02.2016.
  */
@@ -12,24 +10,24 @@ public class UserService {
 
     private IRepository<User> repository;
 
-    public UserService(IRepository<User> userList){
-        setUserList(userList);
+    public UserService(IRepository<User> userRepository){
+        setUserRepository(userRepository);
     }
 
     /**
-     * Returns an IRepository object with all users.
-     * @return An IRepository object with all users.
+     * Returns an list with all users.
+     * @return An list with all users.
      */
-    public IRepository<User> getUserList() {
-        return repository;
+    public List<User> getUserList() {
+        return Collections.unmodifiableList(repository.getAll());
     }
 
     /**
      * Sets the IRepository object with all users.
-     * @param userList The new IRepository object with users.
+     * @param userRepository The new IRepository object with users.
      */
-    private void setUserList(IRepository<User> userList) {
-        this.repository = userList;
+    private void setUserRepository(IRepository<User> userRepository) {
+        this.repository = userRepository;
     }
 
     public List<User> getAdministrators(){
@@ -44,4 +42,26 @@ public class UserService {
         return Collections.unmodifiableList(repository.getAllMatching((s)->s instanceof Developer));
     }
 
+
+    // is de user name uniek?
+    public void addUser(User user){
+        String userName =  user.getUserName();
+        if (!repository.getAllMatching((s)->s.getUserName() == userName).isEmpty()){
+            throw new IllegalArgumentException("This user already exists. Choose another user name.");
+        } else {
+            repository.insert(user);
+        }
+    }
+
+    public void updateUser(User user){
+        repository.update(((s)->s.getUserName() == user.getUserName()), user);
+    }
+
+    public void deleteUser(User user){
+        repository.delete(user);
+    }
+
+    public User getUser(String userName){
+        return repository.getOne((s)->s.getUserName()==userName);
+    }
 }
