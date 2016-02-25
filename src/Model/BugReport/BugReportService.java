e7e37925736d50e35d299c7ea080a79917634760
Tom Houben <tomhouben@telenet.be>
2016-02-25 11:25:08 +0100

    Bugreport service further implemented

--- src/Model/BugReport/BugReportService.java
+++ src/Model/BugReport/BugReportService.java
@@ -1,6 +1,11 @@
 package Model.BugReport;
 
 import DAL.IRepository;
+import Model.Project;
+import Model.SubSystem;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by Tom on 24/02/16.
@@ -17,4 +22,64 @@ public class BugReportService {
     public BugReportService(IRepository<BugReport> bugReportRepository){
         this.bugReportRepository = bugReportRepository;
     }
+
+    /**
+     * Getter to request all the BugReports there are.
+     *
+     * @return A list of all the BugReports.
+     */
+    public List<BugReport> getAllBugReports(){
+        return Collections.unmodifiableList(this.bugReportRepository.getAll());
+    }
+
+    /**
+     * Getter to get one specific BugReport.
+     *
+     * @param id The id of the BugReport to get.
+     *
+     * @return The BugReport matching the given id.
+     */
+    public BugReport getBugReport(BugReportID id){
+        return this.bugReportRepository.getOne(x -> x.getId().equals(id));
+    }
+
+    /**
+     * Method for adding a new BugReport.
+     *
+     * @param bugReport The BugReport to Add.
+     */
+    public void addBugReport(BugReport bugReport){
+        this.bugReportRepository.insert(bugReport);
+    }
+
+    /**
+     * Method for updating a BugReport if the id matches no bugReport the bugReport will be added as new bugReport.
+     *
+     * @param id The id of the bugreport to update
+     *
+     * @param bugReport The updated bugreport.
+     */
+    public void updateBugReport(BugReportID id, BugReport bugReport){
+        this.bugReportRepository.update(x -> x.getId().equals(id), bugReport);
+    }
+
+    /**
+     * Method for deleting a BugReport.
+     *
+     * @param bugReport The bugreport to delete.
+     */
+    public void deleteBugReport(BugReport bugReport){
+        this.bugReportRepository.delete(bugReport);
+    }
+
+    /**
+     * Getter for retrieving all bugReports concerning the given project.
+     *
+     * @param project The project for which to find the bugReports.
+     */
+    public void getBugReportsForProject(Project project){
+        List<SubSystem> subsystems = project.getAllSubSystem();
+        this.bugReportRepository.getAllMatching(x -> subsystems.contains(x.getSubsystem()));
+    }
+
 }
