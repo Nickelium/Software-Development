06477f7c719565024a23bf3eec063e6a0987f53a
Karina Karapetyan <karina.st.vith@gmail.com>
2016-02-26 11:43:54 +0100

    in UserService: Repository -> ListWrapper

--- src/Model/User/UserService.java
+++ src/Model/User/UserService.java
@@ -1,18 +1,23 @@
 package Model.User;
+import DAL.IListWrapper;
+import DAL.ListWrapper;
+
 import java.util.Collections;
 import java.util.List;
 
 /**
  * Created by Karina on 19.02.2016.
  */
-public class UserService {
+public abstract class UserService {
 
-    private IRepository<User> repository;
+    private IListWrapper<User> userList;
 
+    /**
+     * Creates a new UserService with an empty ListWrapper.
+     */
     public UserService(){
-        // TODO add generic Repository
-        // IRepository<User> userRepository = new Repository<User>
-        // setUserRepository(userRepository);
+        IListWrapper<User> userList = new ListWrapper<User>();
+        setListWrapper(userList);
     }
 
     /**
@@ -20,45 +25,45 @@ public class UserService {
      * @return An list with all users.
      */
     public List<User> getUserList() {
-        return Collections.unmodifiableList(repository.getAll());
+        return Collections.unmodifiableList(userList.getAll());
     }
 
     /**
-     * Sets the IRepository object with all users.
-     * @param userRepository The new IRepository object with users.
+     * Sets the IListWrapper object with all users.
+     * @param userList The new IListWrapper object with users.
      */
-    private void setUserRepository(IRepository<User> userRepository) {
-        this.repository = userRepository;
+    private void setListWrapper(IListWrapper<User> userList) {
+        this.userList = userList;
     }
 
     /**
-     * Prompts a query to the UserRepository returning all users being an instance of Admin
-     * @return A list object with all administrators saved in the UserRepository
+     * Prompts a query to the User List returning all users being an instance of Admin
+     * @return A list object with all administrators saved in the User List.
      */
     public List<User> getAdministrators(){
-        return Collections.unmodifiableList(repository.getAllMatching((s)->s instanceof Admin));
+        return Collections.unmodifiableList(userList.getAllMatching((s)->s instanceof Admin));
     }
 
     /**
-     * Prompts a query to the UserRepository returning all users being an instance of issuer
+     * Prompts a query to the User List returning all users being an instance of issuer
      * @return A list object with all issuers saved in the UserRepository
      */
     public List<User> getIssuers(){
-        return Collections.unmodifiableList(repository.getAllMatching((s)->s instanceof Issuer));
+        return Collections.unmodifiableList(userList.getAllMatching((s)->s instanceof Issuer));
     }
 
     /**
-     * Prompts a query to the UserRepository returning all users being an instance of Developer
+     * Prompts a query to the User List returning all users being an instance of Developer
      * @return A list object with all developers saved in the UserRepository
      */
     public List<User> getDeveloper(){
-        return Collections.unmodifiableList(repository.getAllMatching((s)->s instanceof Developer));
+        return Collections.unmodifiableList(userList.getAllMatching((s)->s instanceof Developer));
     }
 
 
     /**
-     * Adds a new user to the user repository if there doesn't exist a user object with
-     * the same username in the User Repository yet.
+     * Adds a new user to the user userList if there doesn't exist a user object with
+     * the same username in the User List yet.
      *
      * @throws IllegalArgumentException
      * Throws an IllegalArgumentException in case there already exists a User object
@@ -73,18 +78,18 @@ public class UserService {
      */
     public User addUser(String firstName, String lastName, String userName, String middleName){
         User user = new User(firstName,lastName,userName,middleName);
-        if (!repository.getAllMatching((s)->user.getUserName() == userName).isEmpty()){
+        if (!userList.getAllMatching((s)->user.getUserName() == userName).isEmpty()){
             throw new IllegalArgumentException("This user already exists. Choose another user name.");
         } else {
-            repository.insert(user);
+            userList.insert(user);
             return user;
         }
     }
 
 
     /**
-     * Adds a new user to the user repository if there doesn't exist a user object with
-     * the same username in the User Repository yet.
+     * Adds a new user to the user userList if there doesn't exist a user object with
+     * the same username in the User List yet.
      *
      * @throws IllegalArgumentException
      * Throws an IllegalArgumentException in case there already exists a User object
@@ -98,32 +103,32 @@ public class UserService {
      */
     public User addUser(String firstName, String lastName, String userName){
         User user = new User(firstName,lastName,userName);
-        if (!repository.getAllMatching((s)->user.getUserName() == userName).isEmpty()){
+        if (!userList.getAllMatching((s)->user.getUserName() == userName).isEmpty()){
             throw new IllegalArgumentException("This user already exists. Choose another user name.");
         } else {
-            repository.insert(user);
+            userList.insert(user);
             return user;
         }
     }
 
 
     /**
-     * Deletes a given user object from the user repository by calling a delete
-     * statement of the repository.
+     * Deletes a given user object from the user userList by calling a delete
+     * statement of the userList.
      *
-     * @param user the user object that needs to be deleted from the user repository.
+     * @param user the user object that needs to be deleted from the user userList.
      */
     public void deleteUser(User user){
-        repository.delete(user);
+        userList.delete(user);
     }
 
     /**
-     * Returns a user object from the user repository defined by the unique user name.
+     * Returns a user object from the user userList defined by the unique user name.
      *
-     * @param userName the unique username of which we want to retrieve the user object from the user repository
-     * @return the user object from the user repository with given username.
+     * @param userName the unique username of which we want to retrieve the user object from the user userList
+     * @return the user object from the user userList with given username.
      */
     public User getUser(String userName){
-        return repository.getOne((s)->s.getUserName()==userName);
+        return userList.getOne((s)->s.getUserName()==userName);
     }
 }
