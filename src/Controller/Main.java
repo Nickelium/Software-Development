0bb730a3f2cd2758182b2638ab9194effaa66b64
Tom Houben <tomhouben@telenet.be>
2016-02-28 14:03:31 +0100

    Mode controller files

---
+++ src/Controller/Main.java
@@ -0,0 +1,13 @@
+package Controller;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		MainController mc = new MainController();
+		mc.run();
+
+	}
+
+}
