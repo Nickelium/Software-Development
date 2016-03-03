e370cdee4ca91cb81e8bdea08b21a1c4f1ee3d87
Karina Karapetyan <karina.st.vith@gmail.com>
2016-03-03 15:39:23 +0100

    Initializer changed

--- src/Controller/IInitializer.java
+++ src/Controller/IInitializer.java
@@ -1,15 +1,20 @@
 package Controller;
 
+import Model.BugReport.BugReportService;
 import Model.Project.Project;
+import Model.Project.ProjectService;
 import Model.User.User;
+import Model.User.UserService;
 import Model.Wrapper.IListWrapper;
 
 public interface IInitializer 
 {
 	public void init();
-	
-	public IListWrapper<Project> getListWrapperProject();
-	
-	public IListWrapper<User> getListWrapperUser();
-	
+
+	public UserService getUserService();
+
+	public ProjectService getProjectService();
+
+	public BugReportService getBugReportService();
+
 }
--- src/Controller/Initializer.java
+++ src/Controller/Initializer.java
@@ -1,44 +1,50 @@
 package Controller;
 
+import Model.BugReport.BugReportService;
 import Model.Project.Project;
+import Model.Project.ProjectService;
 import Model.User.*;
 import Model.Wrapper.IListWrapper;
 import Model.Wrapper.ListWrapper;
 
 public class Initializer implements IInitializer
 {
-
 	private UserService userService;
-	
-	@Override
-	public void init() 
-	{
-		/* 1) we gaan hier niet via service :: aanname dat initieel toestand sowieso correct moet zijn.
-		   2) meerdere services kunnen dezelfde repository/listwrapper bevatten -> dus eerder initializeren van 
-		   		repository/listwrapper dan service + elke controller beslist zelf wat hij als service nodig heeft en dus
-		   		zelf aanmaakt -> gewoon repository/listwrapper doorgeven
-		*/
+	private ProjectService projectService;
+	private BugReportService bugReportService;
 
-		UserService userService = new UserService();
+	public void init(){
+		this.userService = new UserService();
+		// refactor ProjectService class first
+		this.projectService = new ProjectService();
+		this.bugReportService = new BugReportService();
 
-		userService.addAdmin("Frederick","Curtis","curt", "Sam");
-		userService.addIssuer("John","","Doctor","doc");
-		userService.addIssuer("Charles","Arnold","Berg","charlie");
-		userService.addDeveloper("Joseph","","Mays","major");
-		userService.addDeveloper("Maria","","Carney","maria");
-		
+		// init users
+		getUserService().addAdmin("Frederick","Curtis","curt", "Sam");
+		getUserService().addIssuer("John","","Doctor","doc");
+		getUserService().addIssuer("Charles","Arnold","Berg","charlie");
+		getUserService().addDeveloper("Joseph","","Mays","major");
+		getUserService().addDeveloper("Maria","","Carney","maria");
+
+		// creat projects
+		// TODO zal er een creator zijn voor Projects? zo ja, hier gebruiken dan
+
+		// create bugreports
+		// TODO na het aanmaken van de projects
+
 	}
 
-	@Override
-	public IListWrapper<Project> getListWrapperProject()
-	{
-		return listWrapperProject;
+	public UserService getUserService() {
+		return userService;
 	}
 
-	@Override
-	public IListWrapper<User> getListWrapperUser()
-	{
-		return listWrapperUser;
+	public ProjectService getProjectService() {
+		return projectService;
 	}
+
+	public BugReportService getBugReportService() {
+		return bugReportService;
+	}
+
 
 }
