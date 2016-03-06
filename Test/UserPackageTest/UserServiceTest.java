ce893da75340f474b631deae51f5736b6374cdbc
Laurens Cleemput <laurenscleemput@hotmail.com>
2016-03-06 19:20:38 +0100

    Added UserService test class. TODO: implement class

---
+++ Test/UserPackageTest/UserServiceTest.java
@@ -0,0 +1,64 @@
+package UserPackageTest;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Laurens on 6/03/2016.
+ */
+public class UserServiceTest {
+
+    //TODO: To be implemented
+
+    @Before
+    public void initialization() throws Exception {
+
+    }
+
+    @Test
+    public void testGetUserList() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAdministrators() throws Exception {
+
+    }
+
+    @Test
+    public void testGetIssuers() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDevelopers() throws Exception {
+
+    }
+
+    @Test
+    public void testAddAdmin() throws Exception {
+
+    }
+
+    @Test
+    public void testAddIssuer() throws Exception {
+
+    }
+
+    @Test
+    public void testAddDeveloper() throws Exception {
+
+    }
+
+    @Test
+    public void testIsValidUserName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetUser() throws Exception {
+
+    }
+}
