9f779d805e748b071169f6062a67071a8d550827
Tom Houben <tomhouben@telenet.be>
2016-03-09 22:12:50 +0100

    Select bugreports: no bugreports found -> keep iterating till succes added

--- src/Controller/UserController/IssuerController.java
+++ src/Controller/UserController/IssuerController.java
@@ -19,27 +19,26 @@ import java.util.List;
 /**
  * Created by Karina on 05.03.2016.
  */
-public class IssuerController extends UserController{
+public class IssuerController extends UserController {
 
-    public IssuerController(UI ui, UserService userService, ProjectService projectService, BugReportService bugReportService, User currentUser){
+    public IssuerController(UI ui, UserService userService, ProjectService projectService, BugReportService bugReportService, User currentUser) {
         super(ui, userService, projectService, bugReportService, currentUser);
         initializeUseCasesIssuer();
     }
 
-    private void initializeUseCasesIssuer(){
+    private void initializeUseCasesIssuer() {
         try {
             useCases.add(new FunctionWrap("Create Bug Report", IssuerController.class.getMethod("createBugReport")));
             useCases.add(new FunctionWrap("Inspect Bug Report", IssuerController.class.getMethod("inspectBugReport")));
             useCases.add(new FunctionWrap("Create Comment", IssuerController.class.getMethod("createComment")));
 
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
 
-    public void createBugReport()
-    {
+    public void createBugReport() {
         try {
             // select a project
             getUi().display("Select a project:");
@@ -73,12 +72,12 @@ public class IssuerController extends UserController{
             // show all dependencies
             List<BugReport> possibleDependencies = getBugReportService().getBugReportsForProject(project);
             String possibleDependenciesStr = Parser.parseBugReportList(possibleDependencies);
-            while(true){
+            while (true) {
                 getUi().display("\n");
                 getUi().display("Select a possible dependency (enter -1 to exit): ");
                 getUi().display(possibleDependenciesStr);
                 int indexDep = getUi().readInt();
-                if (indexDep ==-1) break;
+                if (indexDep == -1) break;
                 // add dependency
                 BugReport newDependency = possibleDependencies.get(indexDep);
                 bugReport.addDependency(newDependency);
@@ -87,7 +86,7 @@ public class IssuerController extends UserController{
             }
 
             // show all dependencies of this bug report
-        }catch(ModelException | IndexOutOfBoundsException e){
+        } catch (ModelException | IndexOutOfBoundsException e) {
             getUi().errorDisplay(e.getMessage());
             getUi().display("Enter 1 if you want to retry.");
             if (getUi().readInt() == 1) createBugReport();
@@ -95,150 +94,139 @@ public class IssuerController extends UserController{
 
     }
 
-    protected BugReport selectBugReport()
-    {
-    	try 
-    	{
-               int chosenNumber;
-               List<BugReport> bugReportList;
+    protected BugReport selectBugReport() {
+        try {
+            int chosenNumber;
+            List<BugReport> bugReportList = null;
+            boolean ValidSelectionDisplayed = false;
+            // return for usage in DeveloperController
+            while (!ValidSelectionDisplayed) {
+                getUi().display("Select the preferred search method: ");
+                getUi().display(Parser.parseSearchMethods());
 
-               // return for usage in DeveloperController
+                int methodIndex = getUi().readInt();
 
-               getUi().display("Select the preferred search method: ");
-               getUi().display(Parser.parseSearchMethods());
 
-               int methodIndex = getUi().readInt();
+                if (methodIndex == 0) {
+                    // Search for bug reports with a specific string in the title or description
+                    getUi().display("Please enter a search string matching the title or description of the desired bug report.");
+                    String query = getUi().readString();
 
-               if(methodIndex == 0){
-                   // Search for bug reports with a specific string in the title or description
-                   getUi().display("Please enter a search string matching the title or description of the desired bug report.");
-                   String query = getUi().readString();
+                    // Make List with possible bug reports
+                    List<BugReport> list1 = getBugReportService().getBugReportsWithDescriptionContaining(query);
+                    List<BugReport> list2 = getBugReportService().getBugReportsWithTitleContaining(query);
 
-                   // Make List with possible bug reports
-                   List<BugReport> list1 = getBugReportService().getBugReportsWithDescriptionContaining(query);
-                   List<BugReport> list2 = getBugReportService().getBugReportsWithTitleContaining(query);
+                    // Combine both lists
+                    bugReportList = new ArrayList<BugReport>(list1);
+                    for (BugReport b : list2) {
+                        bugReportList.add(b);
+                    }
 
-                   // Combine both lists
-                   bugReportList = new ArrayList<BugReport>(list1);
-                   for(BugReport b : list2){
-                       bugReportList.add(b);
-                   }
+                    if (bugReportList.size() > 0) {
+                        // Show Results
+                        getUi().display("The search result for your query is: ");
+                        getUi().display(Parser.parseBugReportList(bugReportList));
+                        ValidSelectionDisplayed = true;
+                    } else {
+                        getUi().display("No bug reports found.");
+                    }
 
-                   if(bugReportList.size()>0) {
-                       // Show Results
-                       getUi().display("The search result for your query is: ");
-                       getUi().display(Parser.parseBugReportList(bugReportList));
-                   }else{
-                       getUi().display("No bug reports found.");
-                   }
 
-               }
+                } else if (methodIndex == 1) {
 
-               else if (methodIndex == 1){
-                   // Search for bug reports filed by some specific user
-                   getUi().display("Please enter the username of the user that filed the desired bug report: ");
-                   String userName = getUi().readString();
+                    // Search for bug reports filed by some specific user
+                    getUi().display("Please enter the username of the user that filed the desired bug report: ");
+                    String userName = getUi().readString();
 
-                   User user = getUserService().getUser(userName);
-                   bugReportList = getBugReportService().getBugReportsFiledByUser(user);
-                   if(bugReportList.size() > 0) {
-                       // Show Results
-                       getUi().display("The search result for your query is: ");
-                       getUi().display(Parser.parseBugReportList(bugReportList));
-                   }else{
-                       getUi().display("No bug reports found.");
-                   }
-               }
+                    User user = getUserService().getUser(userName);
+                    bugReportList = getBugReportService().getBugReportsFiledByUser(user);
+                    if (bugReportList.size() > 0) {
+                        // Show Results
+                        getUi().display("The search result for your query is: ");
+                        getUi().display(Parser.parseBugReportList(bugReportList));
+                        ValidSelectionDisplayed = true;
+                    } else {
+                        getUi().display("No bug reports found.");
 
-               else {
-                   // Search for bug reports assigned to specific user
-                   getUi().display("Please enter the username of the user that the bug reports are assigned to: ");
-                   String userName = getUi().readString();
+                    }
+                } else {
+                    // Search for bug reports assigned to specific user
+                    getUi().display("Please enter the username of the user that the bug reports are assigned to: ");
+                    String userName = getUi().readString();
 
-                   User user = getUserService().getUser(userName);
-                   bugReportList = getBugReportService().getBugReportsAssignedToUser(user);
+                    User user = getUserService().getUser(userName);
+                    bugReportList = getBugReportService().getBugReportsAssignedToUser(user);
 
-                   // Show Results
-                   getUi().display("The search result for your query is: ");
-                   getUi().display(Parser.parseBugReportList(bugReportList));
-               }
-
-               // Make choice
-               getUi().display("Please enter the number of the bug report that you would like to select: ");
-               chosenNumber = getUi().readInt();
-
-               return bugReportList.get(chosenNumber);
-    	  }
-        catch (ModelException | IndexOutOfBoundsException e) 
-    	{
-        	getUi().errorDisplay(e.getMessage());
+                    if (bugReportList.size() > 0) {
+                        // Show Results
+                        getUi().display("The search result for your query is: ");
+                        getUi().display(Parser.parseBugReportList(bugReportList));
+                        ValidSelectionDisplayed = true;
+                    } else {
+                        getUi().display("No bug reports found.");
+                    }
+                }
+            }
+            // Make choice
+            getUi().display("Please enter the number of the bug report that you would like to select: ");
+            chosenNumber = getUi().readInt();
+            return bugReportList.get(chosenNumber);
+        } catch (ModelException | IndexOutOfBoundsException e) {
+            getUi().errorDisplay(e.getMessage());
             //TODO wat gaan we hier doen als het niet werkt??
             e.printStackTrace();
         }
         return null;
     }
 
-    public void inspectBugReport()
-    {
-    	try
-    	{
-    		BugReport bugReport = selectBugReport();	    	
-	    	String bugReportDetails = Parser.parseDetailBugReport(bugReport);
-	    	getUi().display(bugReportDetails);
-    	}
-    	catch(IndexOutOfBoundsException e)
-    	{
-    		 getUi().errorDisplay(e.getMessage());
-             getUi().display("Enter 1 if you want to retry.");
-             if (getUi().readInt() == 1) inspectBugReport();
-    	}
+    public void inspectBugReport() {
+        try {
+            BugReport bugReport = selectBugReport();
+            String bugReportDetails = Parser.parseDetailBugReport(bugReport);
+            getUi().display(bugReportDetails);
+        } catch (IndexOutOfBoundsException e) {
+            getUi().errorDisplay(e.getMessage());
+            getUi().display("Enter 1 if you want to retry.");
+            if (getUi().readInt() == 1) inspectBugReport();
+        }
 
     }
 
-    public void createComment()
-    {
-    	try
-    	{
-    		BugReport bugReport = selectBugReport();
-    		getUi().display("List of all comments of this bugreport:");
-    		List<Comment> listComment = bugReport.getAllComments();
-    		String parsedListComment = Parser.parseCommentList(listComment);
-    		getUi().display(parsedListComment);
+    public void createComment() {
+        try {
+            BugReport bugReport = selectBugReport();
+            getUi().display("List of all comments of this bugreport:");
+            List<Comment> listComment = bugReport.getAllComments();
+            String parsedListComment = Parser.parseCommentList(listComment);
+            getUi().display(parsedListComment);
 
             //whaaat
-    		getUi().display("Create a comment of the bugreport or on one of the comments (B/C) : ");
-	        String input = getUi().readString();
+            getUi().display("Create a comment of the bugreport or on one of the comments (B/C) : ");
+            String input = getUi().readString();
 
-            if (input.equalsIgnoreCase("b")) 
-            {
+            if (input.equalsIgnoreCase("b")) {
                 getUi().display("Comment:");
                 String text = getUi().readString();
-                Comment newComment = getBugReportService().createComment(text, (Issuer)getCurrentUser(), bugReport);
+                Comment newComment = getBugReportService().createComment(text, (Issuer) getCurrentUser(), bugReport);
 
 
-            } 
-            else if (input.equalsIgnoreCase("c")) 
-            {
+            } else if (input.equalsIgnoreCase("c")) {
                 int index;
                 getUi().display("Choose a comment from one of above: ");
                 index = getUi().readInt();
                 Comment comm = listComment.get(index);
                 getUi().display("Comment:");
                 String text = getUi().readString();
-                Comment newComment = getBugReportService().createComment(text, (Issuer)getCurrentUser(), comm);
-             }
-            else
-            {
-               throw new ModelException("This is an invalid input");
+                Comment newComment = getBugReportService().createComment(text, (Issuer) getCurrentUser(), comm);
+            } else {
+                throw new ModelException("This is an invalid input");
             }
             getUi().display("The comment has been successfully created.\n");
-    	}
-    	catch(ModelException | IndexOutOfBoundsException e)
-    	{
-    		 getUi().errorDisplay(e.getMessage());
-             getUi().display("Enter 1 if you want to retry.");
-             if (getUi().readInt() == 1) createComment();
-    	}
+        } catch (ModelException | IndexOutOfBoundsException e) {
+            getUi().errorDisplay(e.getMessage());
+            getUi().display("Enter 1 if you want to retry.");
+            if (getUi().readInt() == 1) createComment();
+        }
     }
 
 }
