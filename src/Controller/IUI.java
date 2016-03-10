c0302f00a104f046c2188044d43b29afbac4bf10
Karina Karapetyan <karina.st.vith@gmail.com>
2016-03-10 18:54:42 +0100

    IUI

---
+++ src/Controller/IUI.java
@@ -0,0 +1,20 @@
+package Controller;
+
+/**
+ * Created by Karina on 10.03.2016.
+ */
+public interface IUI {
+    void display(String str);
+
+    String readString();
+
+    String readLine();
+
+    String readMultiline();
+
+    double readDouble();
+
+    int readInt();
+
+    void errorDisplay(String str);
+}
