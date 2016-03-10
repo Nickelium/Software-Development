baf48cf8f2f17a6ada9e373d4b99060d4a6337ee
Karina Karapetyan <karina.st.vith@gmail.com>
2016-03-10 23:00:06 +0100

    Fork Project added

--- Test/UseCaseTests/AdminControllerTest/ForkProject.java
+++ Test/UseCaseTests/AdminControllerTest/ForkProject.java
@@ -1,94 +1,103 @@
 package UseCaseTests.AdminControllerTest;
 
-import Controller.IUI;
-import Controller.LoginController;
 import Controller.UserController.AdminController;
 import Controller.UserController.UserController;
 import CustomExceptions.ModelException;
-import UseCaseTests.InitializerTest;
 import UseCaseTests.UseCasesUI.TestUI;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import static org.junit.Assert.*;
-
 /**
  * Created by Karina on 10.03.2016.
  */
-public class CreateProject extends AdminControllerInit {
+public class ForkProject  extends AdminControllerInit {
 
     @Test
-    public void successfullyCreatedProject() throws Exception{
+    public void successfulForkedProject() throws Exception{
         String[] simulatedUserInput = {
-                "Project Test",
-                "Project Test Discription",
-                ".",
-                "11/12/2016",
+                "0",
+                "1.1",
+                "11/11/2016",
                 "200.0",
-                "0"
+                "1"
         };
         ArrayList<String> input = new ArrayList<>(Arrays.asList(simulatedUserInput));
         TestUI ui = new TestUI(input);
 
         UserController adminController = new AdminController(ui,userService,projectService,bugReportService,loginController.getCurrentUser());
-        adminController.callUseCase(2);
+        adminController.callUseCase(3);
     }
 
+
     @Test(expected = ModelException.class)
-    public void unsuccessfulCreateProject_invalidDate() throws Exception{
+    public void unsuccessfullyForkProject_invalidVersionID() throws Exception{
         String[] simulatedUserInput = {
-                "Project Test",
-                "Project Test Discription",
-                ".",
-                "11/12/2014",
+                "0",
+                "0.3",
+                "11/11/2016",
                 "200.0",
-                "0"
+                "1"
         };
         ArrayList<String> input = new ArrayList<>(Arrays.asList(simulatedUserInput));
         TestUI ui = new TestUI(input);
 
         UserController adminController = new AdminController(ui,userService,projectService,bugReportService,loginController.getCurrentUser());
-        adminController.callUseCase(2);
+        adminController.callUseCase(3);
+
+    }
+
+    @Test(expected = ModelException.class)
+    public void unsuccessfullyForkProject_invalidStartingDate() throws Exception{
+        String[] simulatedUserInput = {
+                "0",
+                "1.1",
+                "11/11/2014",
+                "200.0",
+                "1"
+        };
+        ArrayList<String> input = new ArrayList<>(Arrays.asList(simulatedUserInput));
+        TestUI ui = new TestUI(input);
+
+        UserController adminController = new AdminController(ui,userService,projectService,bugReportService,loginController.getCurrentUser());
+        adminController.callUseCase(3);
+
     }
 
 
     @Test(expected = ModelException.class)
-    public void unsuccessfulCreateProject_invalidBudget() throws Exception{
+    public void unsuccessfullyForkProject_invalidBudget() throws Exception{
         String[] simulatedUserInput = {
-                "Project Test",
-                "Project Test Discription",
-                ".",
-                "11/12/2016",
+                "0",
+                "1.1",
+                "11/11/2016",
                 "-200.0",
-                "0"
+                "1"
         };
         ArrayList<String> input = new ArrayList<>(Arrays.asList(simulatedUserInput));
         TestUI ui = new TestUI(input);
 
         UserController adminController = new AdminController(ui,userService,projectService,bugReportService,loginController.getCurrentUser());
-        adminController.callUseCase(2);
+        adminController.callUseCase(3);
 
     }
-
 
     @Test(expected = IndexOutOfBoundsException.class)
-    public void unsuccessfulCreateProject_invalidLead() throws Exception{
+    public void unsuccessfullyForkProject_invalidLead() throws Exception{
         String[] simulatedUserInput = {
-                "Project Test",
-                "Project Test Discription",
-                ".",
-                "11/12/2016",
-                "-200.0",
+                "0",
+                "1.1",
+                "11/11/2016",
+                "200.0",
                 "5"
         };
         ArrayList<String> input = new ArrayList<>(Arrays.asList(simulatedUserInput));
         TestUI ui = new TestUI(input);
 
         UserController adminController = new AdminController(ui,userService,projectService,bugReportService,loginController.getCurrentUser());
-        adminController.callUseCase(2);
+        adminController.callUseCase(3);
+
     }
 
 }
