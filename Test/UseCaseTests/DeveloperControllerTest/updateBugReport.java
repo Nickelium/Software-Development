217aa71c50359b27a8738e4ebc3e57a68eca56f5
Tom Houben <tomhouben@telenet.be>
2016-03-11 01:27:57 +0100

    Try catch removed

--- Test/UseCaseTests/DeveloperControllerTest/updateBugReport.java
+++ Test/UseCaseTests/DeveloperControllerTest/updateBugReport.java
@@ -10,38 +10,54 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
- * Created by Tom on 10/03/16.
+ * Created by Tom on 11/03/16.
  */
-public class assignTobugReport extends DeveloperTestInitializer {
+public class updateBugReport extends DeveloperTestInitializer {
     @Test
-    public void successfullyAssignedDeveloperToBugReport() throws Exception {
+    public void successfullyUpdatedBugReport() throws Exception {
         String[] simulatedUserInput = {
                 "0",
                 "Crash",
                 "0",
-                "0",
+                "UnderReview"
         };
 
         ArrayList<String> input = new ArrayList<String>(Arrays.asList(simulatedUserInput));
         IUI ui = new TestUI(input);
 
         DeveloperController developerController = new DeveloperController(ui, userService, projectService, bugReportService, userService.getUser("major"), developerAssignmentService, tagAssignmentService);
-        developerController.callUseCase(6);
+        developerController.callUseCase(7);
     }
 
     @Test(expected = ModelException.class)
-    public void unsuccessfullyAssignedDeveloperToBugReport() throws Exception {
+    public void unSuccessfullyUpdatedBugReport_IllegalUser() throws Exception {
         String[] simulatedUserInput = {
                 "0",
                 "Crash",
                 "0",
+                "UnderReview"
         };
 
         ArrayList<String> input = new ArrayList<String>(Arrays.asList(simulatedUserInput));
         IUI ui = new TestUI(input);
 
         DeveloperController developerController = new DeveloperController(ui, userService, projectService, bugReportService, userService.getUser("test1"), developerAssignmentService, tagAssignmentService);
-        developerController.callUseCase(6);
+        developerController.callUseCase(7);
     }
 
+    @Test(expected = ModelException.class)
+    public void unSuccessfullyUpdatedBugReport_IllegalTagSwitch() throws Exception {
+        String[] simulatedUserInput = {
+                "0",
+                "parse_ewd",
+                "0",
+                "UnderReview"
+        };
+
+        ArrayList<String> input = new ArrayList<String>(Arrays.asList(simulatedUserInput));
+        IUI ui = new TestUI(input);
+
+        DeveloperController developerController = new DeveloperController(ui, userService, projectService, bugReportService, userService.getUser("major"), developerAssignmentService, tagAssignmentService);
+        developerController.callUseCase(7);
+    }
 }
