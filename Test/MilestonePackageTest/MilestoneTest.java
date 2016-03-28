1fa01bb7352befba09b74ca111d1d25a06c3e72f
Laurens Cleemput <laurenscleemput@hotmail.com>
2016-03-28 23:23:42 +0200

    Consistency guaranteed in milestone class + tests finished.

--- Test/MilestonePackageTest/MilestoneTest.java
+++ Test/MilestonePackageTest/MilestoneTest.java
@@ -2,7 +2,10 @@ package MilestonePackageTest;
 
 import CustomExceptions.ReportErrorToUserException;
 import Model.Milestone.Milestone;
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
@@ -12,8 +15,6 @@ public class MilestoneTest {
 
     @Before
     public void initialization(){
-
-        // Niets
 
     }
 
@@ -25,7 +26,7 @@ public class MilestoneTest {
 
     @Test
     public void isValidIDTest2() throws ReportErrorToUserException {
-        Milestone milestoneValidID = new Milestone("M0.5.6");
+        Milestone milestoneValidID = new Milestone("M0.5.60");
         assertTrue(milestoneValidID.isValidMilestoneID(milestoneValidID.getMilestoneID()));
     }
 
@@ -39,4 +40,51 @@ public class MilestoneTest {
         Milestone milestoneInvalidID2 = new Milestone("M0.5.6/4");
     }
 
+    @Test(expected = ReportErrorToUserException.class)
+    public void isValidIDTest5() throws ReportErrorToUserException{
+        Milestone milestoneInvalidID3 = new Milestone("M0.500.1");
+    }
+
+    @Test
+    public void getIDValueTest1() throws ReportErrorToUserException {
+        Milestone milestone = new Milestone("M0.8.9");
+        assertEquals(0.0809, milestone.getIDvalue(), 0.0000001);
+    }
+
+    @Test
+    public void getIDValueTest2() throws ReportErrorToUserException {
+        Milestone milestone = new Milestone("M0.80.9");
+        assertEquals(0.8009, milestone.getIDvalue(), 0.0000001);
+    }
+
+    @Test
+    public void getIDValueTest3() throws ReportErrorToUserException {
+        Milestone milestone = new Milestone("M0.8.95");
+        assertEquals(0.0895, milestone.getIDvalue(), 0.0000001);
+    }
+
+    @Test
+    public void getIDValueTest4() throws ReportErrorToUserException {
+        Milestone milestone = new Milestone("M0.1.2.3.4.5.6.7.8.9");
+        assertEquals(0.010203040506070809, milestone.getIDvalue(), 0.0000001);
+    }
+
+    @Test
+    public void getIDValueTest5() throws ReportErrorToUserException {
+        Milestone milestone = new Milestone("M1");
+        assertEquals(1.0, milestone.getIDvalue(), 0.0000001);
+    }
+
+    @Test
+    public void getIDValueTest6() throws ReportErrorToUserException {
+        Milestone milestone = new Milestone("M12");
+        assertEquals(12.0, milestone.getIDvalue(), 0.0000001);
+    }
+
+    @Test
+    public void getIDValueTestComparison() throws ReportErrorToUserException{
+        Milestone milestone1 = new Milestone("M3.2.1.0");
+        Milestone milestone2 = new Milestone("M3.2.1.1");
+        assertTrue(milestone1.getIDvalue() < milestone2.getIDvalue());
+    }
 }
--- src/Model/Milestone/Milestone.java
+++ src/Model/Milestone/Milestone.java
@@ -14,10 +14,7 @@ public class Milestone {
     private String milestoneID;
 
     public Milestone(String milestoneID) throws ReportErrorToUserException {
-        if(isValidMilestoneID(milestoneID))
-            this.milestoneID = milestoneID;
-        else
-            throw new ReportErrorToUserException("Invalid milestoneID has been supplied");
+        setMilestoneID(milestoneID);
     }
 
     public Milestone(){
@@ -28,8 +25,11 @@ public class Milestone {
         return milestoneID;
     }
 
-    public void setMilestoneID(String milestoneID) {
-        this.milestoneID = milestoneID;
+    public void setMilestoneID(String milestoneID) throws ReportErrorToUserException {
+        if(isValidMilestoneID(milestoneID))
+            this.milestoneID = milestoneID;
+        else
+            throw new ReportErrorToUserException("Invalid milestoneID has been supplied");
     }
 
     public boolean isValidMilestoneID(String milestoneID){
@@ -42,22 +42,100 @@ public class Milestone {
                 return false;
         }
 
-        // Volledige string is OK!
+        // Er moeten tussen twee punten minstens één digit staan
+        if(!hasDigitBetweenEveryCoupleDots(chars))
+            return false;
+
+        // Einde string moet een digit zijn
+        if(chars[chars.length-1] == '.')
+            return false;
+
+        // Check if max versionnumber < 100
+        if(!isLimitedTo100(chars))
+            return false;
+
+        // Alle testen doorstaan --> valid ID
+        return true;
+    }
+
+    private boolean isLimitedTo100(char[] chars){
+        for(int i=0; i<=chars.length-3; i++){
+            if(isDigit(chars[i]) && isDigit(chars[i+1]) && isDigit(chars[i + 2])){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean hasDigitBetweenEveryCoupleDots(char[] chars){
+        for(int i=0; i<=chars.length-2; i++){
+            if(chars[i] == '.' && chars[i+1] == '.'){
+                return false;
+            }
+        }
         return true;
     }
 
     public double getIDvalue(){
         char[] chars = new char[milestoneID.length()-1];
         milestoneID.getChars(1,milestoneID.length(),chars, 0);
-        int level = 0;
-        double idValue = 0.0;
 
-        for(char c: chars){
-            if(isDigit(c)) {
-                idValue = idValue + (c * Math.pow(10, level));
-                level = level - 1;
-            }
+        double idValue = 0.0;
+        String stringValue = "";
+
+        int i = 0;
+
+        // First Iteration
+        if(isDigit(chars[i]) && chars.length == 1) {
+            stringValue = stringValue + chars[i]  + ".0";
+            i++;
         }
+
+        else if(isDigit(chars[i]) && isDigit(chars[i+1]) && chars.length == 2) {
+            stringValue = stringValue + chars[i] + chars[i + 1] + ".0";
+            i = i+2;
+        }
+
+        else if(isDigit(chars[i]) && isDigit(chars[i+1])) {
+            stringValue = stringValue + chars[i] + chars[i + 1] + ".";
+            i = i+2;
+        }
+
+        else{
+            stringValue = stringValue + chars[i] + ".";
+            i++;
+        }
+
+        // Loop for other part of string
+        while(i<=chars.length-1){
+
+            if(chars[i] == '.') {
+                i = i+1;
+            }
+
+            else if(isDigit((chars[i])) && i == chars.length-1){
+                stringValue = stringValue + "0" + chars[i];
+                i = i+1;
+            }
+
+            else if(isDigit((chars[i])) && chars[i+1] == '.'){
+                stringValue = stringValue + "0" + chars[i];
+                i = i+2;
+            }
+
+            else if(isDigit(chars[i]) && isDigit(chars[i+1])) {
+                stringValue = stringValue + chars[i] + chars[i+1];
+                i = i+2;
+            }
+
+            else{
+                System.out.println("Something else happened");
+                i++;
+            }
+
+        }
+
+        idValue = Double.parseDouble(stringValue);
         return idValue;
     }
 }
