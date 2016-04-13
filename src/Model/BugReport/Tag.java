6ed8f90ed7c1eca801bc5802b6c75e22905a57b3
Tom Houben <tomhouben@telenet.be>
2016-04-13 15:43:27 +0200

    Tag Documented

--- src/Model/BugReport/Tag.java
+++ src/Model/BugReport/Tag.java
@@ -58,7 +58,7 @@ public abstract class Tag{
     }
 
     /**
-     * Protected method for changing a tag of some bug report.
+     * General method for changing a tag of some bug report. Can be overridden.
      *
      * @param bugReport the bug report of which the tag has to be changed
      * @param tag the new tag for the specified bug report
@@ -69,23 +69,49 @@ public abstract class Tag{
         tag.updateTagSpecificFields(bugReport);
     }
 
+    /**
+     * General method to update fields of the bugreport that will be set when bugreport is set to this tag.
+     *
+     * @param bugReport The bugreport of which to update the fields.
+     * @throws ReportErrorToUserException Something went wrong during the update of the fields.
+     */
     protected void updateTagSpecificFields(BugReport bugReport) throws ReportErrorToUserException {
     }
 
+    /**
+     * Method to let the subclasses have access to remove all the tests from the specified bugreport.
+     * @param bugReport The bugreport of which to remove all the tests.
+     */
     protected final void removeAllTest(BugReport bugReport) {
         bugReport.tests = new ArrayList<>();
     }
 
+    /**
+     * Method to let the subclasses have access to remove all the patches from the specified bugreport.
+     * @param bugReport The bugreport of which to remove all the patches.
+     */
     protected final void removeAllPatches(BugReport bugReport) {
         bugReport.patches = new ArrayList<>();
     }
 
+    /**
+     * Method to let the subclasses have access to set the selected patch of the specified bugreport.
+     * @param bugReport The bugreport of which to set the selected patch.
+     * @param indexPath The index of the selected patch in the list of all the patches of that bugreport.
+     * @throws ReportErrorToUserException Something went wrong while selecting the patch with the given index.
+     */
     protected final void setSelectedPatch(BugReport bugReport, int indexPath) throws ReportErrorToUserException {
         if (indexPath >= bugReport.getPatches().size())
             throw new ReportErrorToUserException("Selected patch does not exist in selected bugreport!");
         bugReport.selectedPatch = bugReport.getPatches().get(indexPath);
     }
 
+    /**
+     * Method to let the subclasses have access to set the score of the given solution of the specified bugreport.
+     * @param bugReport The bugreport of which to set the selected patch.
+     * @param score The score to which to set the selected bugreport.
+     * @throws ReportErrorToUserException The score given is not a valid score.
+     */
     protected final void setSolutionScore(BugReport bugReport, int score) throws ReportErrorToUserException {
         if (score <= 0 || score > 5) throw new ReportErrorToUserException("The given score is not valid");
         bugReport.solutionScore = score;
