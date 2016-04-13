79644921391bf1878889bc683a22c5f1018664c7
Laurens Cleemput <laurenscleemput@hotmail.com>
2016-04-13 18:05:39 +0200

    Documentatie deel 4

--- src/Model/BugReport/BugReportMemento.java
+++ src/Model/BugReport/BugReportMemento.java
@@ -7,10 +7,17 @@ import Model.User.Developer;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class provides utility for saving the state of the system at a certain point in time
+ * during execution of the Bug Trap software.
+ *
+ * The bug report memento saves the state of the following attributes:
+ * tag, assignees, comments, targetMilestone, tests & patches.
+ *
+ * This class provides package-visible methods to request the values of the saved fields.
+ */
 public class BugReportMemento extends Memento<BugReport>
 {
-
-	//TODO documentatie volledige klasse
 
 	private Tag tag;
 	private List<Developer> assignees;
@@ -33,32 +40,56 @@ public class BugReportMemento extends Memento<BugReport>
 		this.tests = new ArrayList<>(originator.getTests());
 		this.patches = new ArrayList<>(originator.getPatches());
 	}
-	
+
+	/**
+	 * Returns the tag object of the bug report memento
+	 * @return the tag object of the bug report memento
+     */
 	Tag getTag()
 	{
 		return tag;
 	}
 
+	/**
+	 * Returns the list with assignees of the bug report memento
+	 * @return the list with assignees of the bug report memento
+	 */
 	List<Developer> getAssignees()
 	{
 		return assignees;
 	}
-	
+
+	/**
+	 * Returns the list with comments of the bug report memento
+	 * @return the list with comments of the bug report memento
+     */
 	List<Comment> getComments()
 	{
 		return comments;
 	}
-	
+
+	/**
+	 * Returns the target milestone object of the bug report memento
+	 * @return the target milestone object of the bug report memento
+     */
 	TargetMilestone getTargetMilestone()
 	{
 		return targetMilestone;
 	}
-	
+
+	/**
+	 * Returns the list with tests of the bug report memento
+	 * @return the list with tests of the bug report memento
+     */
 	List<Test> getTests()
 	{
 		return tests;
 	}
-	
+
+	/**
+	 * Returns the list with patches of the bug report memento
+	 * @return the list with patches of the bug report memento
+     */
 	List<Patch> getPatches()
 	{
 		return patches;
