c184b060adfc26807fbd66dd9b0248649b444fd1
Laurens Cleemput <laurenscleemput@hotmail.com>
2016-04-13 17:11:19 +0200

    Babycommit

--- src/Model/Mail/Mailbox.java
+++ src/Model/Mail/Mailbox.java
@@ -10,7 +10,8 @@ import Model.Memento.Originator;
 
 /**
  * Class that represents a mailbox of a user.
- *
+ * A mailbox contains all notifications of which a user is registred to.
+ * //TODO Documenteren van Mail package !!!
  */
 public class Mailbox implements Originator<MailboxMemento, Mailbox>
 {
