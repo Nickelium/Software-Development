4e825465a218614bb125430f18b4a8296ca543dd
Laurens Cleemput <laurenscleemput@hotmail.com>
2016-04-15 01:33:39 +0200

    Added & updated SD's up to use case 17

--- src/Controller/UserController/UseCases/IssuerUseCases/CreateBugReport.java
+++ src/Controller/UserController/UseCases/IssuerUseCases/CreateBugReport.java
@@ -34,10 +34,16 @@ public class CreateBugReport extends IssuerUseCase{
      * 5. The issuer selects a subsystem.
      * 6. The system shows the bug report creation form.
      * 7. The issuer enters the bug report details: title and description.
-     * 8. The system shows a list of possible dependencies of this bug report.
-     *    These are the bug reports of the same project.
-     * 9. The issuer selects the dependencies.
-     * 10. The system creates the bug report.
+     * 8. The system asks which of the optional attributes of a bug report the
+     * issuer wants to add: how to reproduce the bug, a stack trace or an
+     * error message.
+     * 9. The issuer enters the selected optional attributes as text.
+     * 10. The system asks if the bug report is private.
+     * 11. The issuer indicates if the bug report is private or not.
+     * 12. The system shows a list of possible dependencies of this bug report.
+     * These are the bug reports of the same project.
+     * 13. The issuer selects the dependencies.
+     * 14. The system creates the bug report.
      *
      * @throws ReportErrorToUserException
      *          in case that the method encounters invalid input
