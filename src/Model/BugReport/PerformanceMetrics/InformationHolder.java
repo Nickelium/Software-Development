fccbadbbdb3f73bb69e08365a9763b2b86a658c7
Laurens Cleemput <laurenscleemput@hotmail.com>
2016-05-07 19:14:59 +0200

    Doc finished (except for leadership)

--- src/Model/BugReport/PerformanceMetrics/InformationHolder.java
+++ src/Model/BugReport/PerformanceMetrics/InformationHolder.java
@@ -16,10 +16,16 @@ public class InformationHolder {
     private String description;
 
     /**
-     * //TODO value zou string moeten zijn, refactoring nodig voor alle uses met information holders
+     * The value of the information that is being held in the information holder.
      */
     private String value;
 
+    /**
+     * Package visible constructor to create a new information holder object.
+     *
+     * @param description the description for the information holder
+     * @param value the value assigned to a specific metric
+     */
     InformationHolder(String description, String value) {
         setDescription(description);
         setValue(value);
@@ -27,19 +33,34 @@ public class InformationHolder {
 
     //region getters & setters
 
-    //TODO
+    /**
+     * Returns the description of an information holder
+     * @return the description field of the information holder object
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Method to set the description of an information holder.
+     * @param description the new description to be set.
+     */
     private void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Returns the value of an information holder
+     * @return the value field of the information holder object
+     */
     public String getValue() {
         return value;
     }
 
+    /**
+     * Method to set the value of an information holder.
+     * @param value the new value to be set.
+     */
     private void setValue(String value) {
         this.value = value;
     }
