e2642771b4c5855b322047fe37ca411cc50823fd
Laurens Cleemput <laurenscleemput@hotmail.com>
2016-05-19 13:18:24 +0200

    Doc Healthindicator package pt1

--- src/Model/HealthIndicator/IHealthIndicator.java
+++ src/Model/HealthIndicator/IHealthIndicator.java
@@ -3,9 +3,13 @@ package Model.HealthIndicator;
 import java.util.List;
 
 /**
- * Created by Tom on 6/05/16.
+ * Interface for health indicator classes
  */
 public interface IHealthIndicator {
 
+    /**
+     * Method to get health indicator components of an object.
+     * @return the applicable health indicator, calculated by a chosen algorithm.
+     */
     List<IHealthIndicator> getDirectHealthIndicatorComponents();
 }
--- src/Model/Milestone/MilestoneContainer.java
+++ src/Model/Milestone/MilestoneContainer.java
@@ -6,15 +6,32 @@ import Model.Project.SubSystem;
 import java.util.List;
 
 /**
- * Created by Tom on 14/04/16.
+ * Interface for milestone container classes.
  */
 public interface MilestoneContainer {
+
+    /**
+     * Method to get all sub systems of a given object.
+     * @return a list with all subsystems
+     */
     List<SubSystem> getAllSubSystems();
 
+    /**
+     * Method to get all milestones of a given object.
+     * @return a list with all milestones
+     */
     List<Milestone> getAllMilestones();
 
+    /**
+     * Method to get all bug reports of a given object.
+     * @return a list with all bug reports
+     */
     List<BugReport> getAllBugReports();
 
+    /**
+     * Method to get the latest achieved milestone of a given object.
+     * @return a milestone object denoting the latest achieved milestone
+     */
     Milestone getLatestAchievedMilestone();
 
 }
